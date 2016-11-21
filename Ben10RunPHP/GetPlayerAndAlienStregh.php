012bdb217c949839c85bb61677d702ad38c002c0
Nathan Vieira <nathanvmag@gmail.com>
2016-11-21 16:31:53 -0200

    Now_Online

--- Ben10RunPHP/GetPlayerAndAlienStregh.php
+++ Ben10RunPHP/GetPlayerAndAlienStregh.php
@@ -1,12 +1,12 @@
 
 <?php
 
-$nickname = $_GET['nick'];
 
-$servername = "localhost";
-$username = "root";
-$password =  null;
-$dbname = "ben10rundb";
+
+$servername = "mysql.hostinger.com.br";
+$username = "u173160052_user";
+$password =  "trakinas00";
+$dbname = "u173160052_ben10";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
@@ -18,8 +18,8 @@ if ($conn->connect_error) {
 $sql = "SELECT p.Nickname as nickname, c.AlienID as alienId, c.Ataque as ataq, a.Nome as aliennome,c.Defesa as def, c.PontosDeVida as hp
             FROM player as p 
             INNER JOIN caixadecapsulas as c ON (p.Nickname = c.Nickname) 
-            INNER JOIN aliens as a ON ( c.AlienID = a.ID)            
-			WHERE (p.Nickname = '$nickname')
+            INNER JOIN aliens as a ON ( c.AlienID = a.ID)           
+			
 			";
 			
 $result = $conn->query($sql);
---
+++ PokemonGo3D/Assets/Scripts/Menu/RegisterScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class RegisterScript : MonoBehaviour
+{
+    [SerializeField]
+    InputField usernameInput;
+    [SerializeField]
+    InputField passwordInput;
+    [SerializeField]
+    InputField emailInput;
+    [SerializeField]
+    Canvas principalCanvas;
+
+	public void OnRegistrar()
+    { 
+        WWWForm form = new WWWForm();
+        form.AddField("nickname", usernameInput.text);
+        form.AddField("senha", passwordInput.text);
+        form.AddField("email", emailInput.text);
+
+		WWW www = new WWW("http://ben10go.96.lt/Register.php", form);
+		Debug.Log (www.text);
+    }
+    public void OnVoltarAoMenu()
+    {
+        this.GetComponent<Canvas>().enabled = false;
+        principalCanvas.enabled = true;
+    }
+
+}
